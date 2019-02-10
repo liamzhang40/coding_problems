513bf126ca0ac0a91770b290d640c92e410341d6
Liangtian Zhang <lzhang40@binghamton.edu>
2019-02-09 20:21:44 -0500

    added delete function to AVL tree

--- treesAndGraphs/AVLTree.js
+++ treesAndGraphs/AVLTree.js
@@ -3,6 +3,48 @@ class AVLTree {
     this.root = null;
   }
 
+  delete(value) {
+    if (this.root) {
+      this.root = this.findNodeAndDelete(this.root, value);
+    } else {
+      return "root is null";
+    }
+  }
+
+  findNodeAndDelete(root, value) {
+    if (!root) return root;
+
+    if (root.value < value) {
+      root.right = this.findNodeAndDelete(root.right, value);
+    } else if (root.value > value) {
+      root.left = this.findNodeAndDelete(root.left, value);
+    } else {
+      if (!root.left && !root.right) {
+        return null;
+      } else if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
+      } else {
+        const tempNode = this.findMaxValueFromLeftSubtree(root.left);
+
+        root.value = tempNode.value;
+        root.left = this.findNodeAndDelete(root.left, tempNode.value);
+
+        return root;
+      }
+    }
+
+    root.height = Math.max(this.getHeight(root.left), this.getHeight(root.right)) + 1;
+    return this.rebalance(root);
+  }
+  
+  findMaxValueFromLeftSubtree(root) {
+    if (!root.right) return root;
+
+    return this.findMaxValueFromLeftSubtree(root.right);
+  }
+
   insert(value) {
     const newNode = new Node(value);
     if (this.root) {
@@ -21,14 +63,29 @@ class AVLTree {
       root.left = this.findLeafAndInsert(root.left, node);
     }
 
+    // backtracking to update the nodes' height
+    root.height = Math.max(this.getHeight(root.left), this.getHeight(root.right)) + 1;
     return this.rebalance(root);
+
+    // if (root.value < node.value) {
+    //   if (root.right) {
+    //     this.findLeafAndInsert(root.right, node);
+    //   } else {
+    //     root.right = node;
+    //   }
+    // } else {
+    //   if (root.left) {
+    //     this.findLeafAndInsert(root.left, node); 
+    //   } else {
+    //     root.left = node;
+    //   }
+    // }
   }
   
   rebalance(root) {
-    // backtracking to update the nodes' height and to balance the unbalanced subtree if necessary
-    // also return the new subtree root
 
-    root.height = Math.max(this.getHeight(root.left), this.getHeight(root.right)) + 1;
+    // backtracking to balance the unbalanced subtree if necessary
+    // also return the new subtree root
     const balance = this.getHeight(root.left) - this.getHeight(root.right);
     let newRoot = root;
     if (balance > 1) {
@@ -166,9 +223,14 @@ tree.insert(3);
 tree.insert(2);
 tree.insert(1);
 tree.insert(0);
-//       4
-//     2   5
-//   1  3
-// 0
+
+tree.delete(4);
+tree.delete(3);
+tree.delete(5);
+//       2
+//     1   4
+//   0    3  5
+// 
+
 console.log(tree);
 console.log(consoleLogTree(tree));
